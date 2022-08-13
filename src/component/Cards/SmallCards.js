6c20af611704da4090564e2677b4bb52b7695897
MAC <user@MACs-MacBook-Pro.local>
2022-08-13 22:00:27 +0100

    first commit

--- src/component/Cards/SmallCards.js
+++ src/component/Cards/SmallCards.js
@@ -1,18 +1,50 @@
-const SmallCard = ({ hotels: { _id, bed, content, createdAt, from, to, image, location, price, title } }) => {
-    
+import { diffDays } from "../../actions";
+import { useNavigate } from 'react-router';
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { Link } from 'react-router-dom';
 
+const SmallCard = ({ hotels: { _id, bed, content, from, to, image, location, price, title } }) => {
+    const navigate = useNavigate();
+    const handleNavigate = (id) => {
+        navigate(`/hotel/${id}`)
+    }
+    const handleHotelDelete = (id) => {
+        console.log(id)
+    }
     return (
        <>
          <div className="card mb-3">
             <div className="row no-gutters">
                 <div className="col-md-4">
-                    <img src="https://via.placeholder.com/900*500.png?text=BOOKING" alt="FAKE" />
+                    {
+                        image && image.contentType ? (
+                            <img className="card-image img img-fluid" src={`http://localhost:7000/api/hotel/image/${_id}`} alt="" />
+                        )
+                        : 
+                        (
+                            <img src="https://via.placeholder.com/900*500.png?text=BOOKING" alt="FAKE" />
+                        )
+                    }
                 </div>
                 <div className="col-md-8">
                     <div className="card-body">
                         <h3 className="card-title">{title} <span className="float-right text-primary">${price}</span></h3>
                         <p className="alert alert-info">{location}</p>
                         <p className="card-text">{`${content.substring(0, 200)}...`}</p>
+                        <p className="card-text">
+                            <span className="float-right text-primary">
+                                for {diffDays(from, to)} {diffDays(from, to) <= 1 ? "day" : "days"}
+                            </span>
+                        </p>
+                        <p className="card-text">{bed}</p>
+                        <p className="card-text">Available from {new Date(from).toLocaleDateString()}</p>
+                        <button onClick={() => handleNavigate(_id)} className="btn btn-primary">Show More</button>
+                        <div className="d-flex justify-content-between h4">
+                            <Link to={`/hotel/edit/${_id}`}>
+                               <EditOutlined className="text-warning" />
+                            </Link>
+                            <DeleteOutlined className="text-danger" onClick={() => handleHotelDelete(_id)} />
+                        </div>
                     </div>
                 </div>
             </div>
